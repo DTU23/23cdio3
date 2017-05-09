d2ee9054f0df368b0da3b03e2e86729c95a6a209
Viktor Poulsen <viktormaigaard@gmail.com>
2017-05-09 13:12:21 +0200

    Implemented API

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -9,30 +9,24 @@ $(document).ready(function () {
         ready: function () {
         }
     });
-    var template = $('#userTableRow').html();
-    var data = {
-        "users":[
-            {"id": 1, "name": "Viktor", "ini": "VP", "cpr": "234567890", "admin": false, "role": "Pharmacist"},
-            {"id": 2, "name": "Christian", "ini": "CN", "cpr": "12345676543", "admin": false, "role": "Operator"},
-            {"id": 3, "name": "Frederik", "ini": "FV", "cpr": "932845893245", "admin": false, "role": "Foreman"},
-            {"id": 4, "name": "Mads", "ini": "MP", "cpr": "34253452241", "admin": true, "role": "None"}
-        ]
-    };
-    Mustache.parse(template);   // optional, speeds up future uses
-    var rendered = Mustache.render(template, data);
-    $('#userListRows').html(rendered).promise().done(function () {
-        $("input:checkbox:not(:checked)").each(function () {
-            $(this).prop("indeterminate", true);
-        });
-    });
+
+    populate();
     
-    $('.edit-user').on("click", function (e) {
+    $(document).on("click", ".edit-user", function (e) {
         var row = $(this).parent().parent();
         template = $('#userEditTemplate').html();
         Mustache.parse(template);   // optional, speeds up future uses
         var rendered = Mustache.render(template, {
-            "id": row.children(".id").text(),
-            "name":row.children(".name").text(),
+            "oprId": row.children(".id").text(),
+            "oprName":row.children(".name").text(),
+            "ini": row.children(".ini").text(),
+            "cpr": row.children(".cpr").text(),
+            "admin": row.children().find(".admin").is(':checked'),
+            "role": row.children(".role").text()
+        });
+        console.log({
+            "oprId": row.children(".id").text(),
+            "oprName":row.children(".name").text(),
             "ini": row.children(".ini").text(),
             "cpr": row.children(".cpr").text(),
             "admin": row.children().find(".admin").is(':checked'),
@@ -44,11 +38,11 @@ $(document).ready(function () {
             select_box.find(option).attr("selected", true);
             console.log(option);
             $('select').material_select();
+            $('#userEditModal').modal('open');
         });
-        $('#userEditModal').modal('open');
     });
 
-    $('.add-user').on("click", function (e) {
+    $(document).on("click", '.add-user', function (e) {
         template = $('#userEditTemplate').html();
         Mustache.parse(template);   // optional, speeds up future uses
         var rendered = Mustache.render(template, {"role": "None", "admin": false});
@@ -63,16 +57,23 @@ $(document).ready(function () {
         Materialize.updateTextFields();
     });
 
-    $('.delete-user').on("click", function (e) {
+    $(document).on("click", '.delete-user', function (e) {
         var row = $(this).parent().parent();
         $.ajax({
             type: "DELETE",
+            contentType: "application/json",
+            processData: false,
+            data: JSON.stringify({}),
             url: "./api/v1/operator/"+row.children(".id").text(),
-            success: function( msg ) {
-                console.log("Response: " + msg);
-                row.fadeOut("slow", function () {
-                    row.remove();
-                });
+            success: function( response ) {
+                if(response){
+                    Materialize.toast("User with ID: "+ row.children(".id").text() +" was deleted!", 4000);
+                    row.fadeOut("slow", function () {
+                        row.remove();
+                    });
+                }else{
+                    Materialize.toast("Unable to delete user!", 4000);
+                }
             },
             error: function ( msg ) {
                 Materialize.toast("Unable to delete user!", 4000);
@@ -87,27 +88,53 @@ $(document).ready(function () {
 
     $('#loginForm').on('submit', function (e) {
         e.preventDefault();
-        $('#loginForm').hide();
-        $('main').removeClass('valign-wrapper');
-        $('#userAdministration').show();
+        $.ajax({
+            type: "POST",
+            data: JSON.stringify({
+                "oprId": 1,
+                "oprName": "Viktor Poulsen",
+                "ini": "vmp",
+                "cpr": "0101892077",
+                "password": "qweqwe",
+                "admin": 1,
+                "role": "pharmacist"
+            }),
+            processData: false,
+            contentType: "application/json",
+            url: "./api/v1/login/",
+            success: function( msg ) {
+                console.log("Response: " + msg);
+                $('#loginForm').hide();
+                $('main').removeClass('valign-wrapper');
+                $('#userAdministration').show();
+            },
+            error: function ( msg ) {
+                Materialize.toast("Invalid login!", 4000);
+                console.log(msg);
+            }
+        });
     });
 
     $(document).on('click', '.modal-save', function (e) {
         e.preventDefault();
         $('#userEditModal').find('.preloader-wrapper').removeClass('hide');
         $.ajax({
-            type: "POST",
-            data: {
-                "id": $('#userEdit').find("#user_id").val(),
-                "name": $('#userEdit').find("#user_name").val(),
+            type: "PUT",
+            contentType: "application/json",
+            processData: false,
+            data: JSON.stringify({
+                "oprId": $('#userEdit').find("#user_id").val(),
+                "oprName": $('#userEdit').find("#user_name").val(),
                 "ini": $('#userEdit').find("#user_ini").val(),
                 "cpr": $('#userEdit').find("#user_cpr").val(),
                 "admin": $('#userEdit').find().find(".admin").is(':checked'),
                 "role": $('#userEdit').find("#user_role").text()
-            },
+            }),
             url: "./api/v1/operator/",
             success: function( msg ) {
                 console.log("Response: " + msg);
+                populate();
+                $('#usereditmodal').modal('close');
             },
             error: function ( msg ) {
                 Materialize.toast("Unable to update user!", 4000);
@@ -123,3 +150,30 @@ $(document).ready(function () {
         $('main').addClass('valign-wrapper');
     })
 });
+
+function populate() {
+    $.ajax({
+        type: "GET",
+        contentType: "application/json",
+        processData: false,
+        url: "./api/v1/operator/",
+        success: function( response ) {
+            console.log(response);
+            var template = $('#userTableRow').html();
+            var data = {};
+            data['users'] = response;
+            Mustache.parse(template);   // optional, speeds up future uses
+            var rendered = Mustache.render(template, data);
+
+            $('#userListRows').html(rendered).promise().done(function () {
+                $("input:checkbox:not(:checked)").each(function () {
+                    $(this).prop("indeterminate", true);
+                });
+            });
+        },
+        error: function ( msg ) {
+            console.log(msg);
+            Materialize.toast("Unable to delete user!", 4000);
+        }
+    });
+}
